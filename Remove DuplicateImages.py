995dab6ddd1b80a4550660f2fe2e11b80b9a08d4
Eviljarro <57341456+Eviljarro@users.noreply.github.com>
2024-10-20 11:14:15 +1000

    Add files via upload
    Added Code that movers images sorted to a different folder. Also adding some comments about where multi threading needs to be implemented as currently it trying to process 20GBs of images it took loner than 12 hours to process.

--- Remove DuplicateImages.py
+++ Remove DuplicateImages.py
@@ -27,13 +27,13 @@ Sorted = []
 skip = False;
 
 ##List all files in Images Directory
-Images = os.listdir("../Images")
+Images = os.listdir("../../TempPictures")
 #print(Images)
 
-
+##Trying to start a threading before this loop, won't work as results from each y comparison impacts the next
 for y in range(len(Images)):
     ##open base image
-    img1 = cv.imread("../Images/"+Images[y], cv.IMREAD_GRAYSCALE)
+    img1 = cv.imread("../../TempPictures/"+Images[y], cv.IMREAD_GRAYSCALE)
 
     ##For every element in Duplicate array, check if Image Y is already in there
     ItemInDuplicates = False;
@@ -46,43 +46,28 @@ for y in range(len(Images)):
     if ItemInDuplicates == False:
        Sorted.append(Images[y])
 
+    ## Could try threading here, as the comparison agaisnt each x is independent, dosen't matter what position x when doing comparison as it won't impact comparison agaisnt next X
     for x in range(y+1,len(Images)):
         #Check if image is being compared agaisnt self and that item hasn't already been found to be in duplicates
         if Images[y] != Images[x] and ItemInDuplicates != True:
-            #open Comparison image
-            img2 = cv.imread("../Images/"+Images[x], cv.IMREAD_GRAYSCALE)
+            try:
+               #open Comparison image
+               img2 = cv.imread("../../TempPictures/"+Images[x], cv.IMREAD_GRAYSCALE)
             
-            ######compare Images########
-            if skip == False:
-                #Check if Image size/shapes are same
-                if img1.shape == img2.shape:
-                    #Perform mean squared error comparison
-                    error = mse(img1, img2)
-                    #open chart if duplicate and display
-                    #print("Image matching Error between the two images:", error)
-                    #plt.figure()
-                    #plt.imshow(img1)
-                    #plt.figure()
-                    #plt.imshow(img2)
-                    #plt.show()
-                    
-                    #cv.waitKey(0)
-                    #cv.destroyAllWindows()
-                    #Append to duplicate list to update jagged array
-                    if error < 1:
-                       Duplicates.append([Images[y],Images[x]])
-                #else:
-                #    print("No Match")
-
-
-
-#print(Duplicates)
-#print(len(Duplicates))
-
-print(Sorted)
-print(len(Sorted))
-
+               ######compare Images########
+               if skip == False:
+                   #Check if Image size/shapes are same
+                   if img1.shape == img2.shape:
+                       #Perform mean squared error comparison
+                       error = mse(img1, img2)
+                       #Append to duplicate list to update jagged array
+                       if error < 1:
+                          Duplicates.append([Images[y],Images[x]])
+            except:
+               print("Can't compare shape of item " + Images[y] + " and item " + Images[x])
 
 #Move itmes on Duplicates List out of folder
-
+#os.rename("C:\files\example.txt", "C:\backup\example.txt")
+for i in range(len(Sorted)):
+   os.rename("../../TempPictures/"+Sorted[i],"../Sorted/"+Sorted[i])
 
